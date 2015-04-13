7cf4708c3965047bebf899e705907f56dc1c9376
Guillermo Villafuerte <gvillafu@comunidad.unam.mx>
2015-04-12 19:54:05 -0500

    Reubicación del landing del sitio.

---
+++ index.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+
+	<title>FLISoL Aragón 2015</title>
+	<meta name="description" content="Página inicial del FLISoL Aragón, a realizarse el 28 de abril de 2015.">
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1"/>
+	<link rel="stylesheet" href="/css/material/base.min.css" />
+	
+</head>
+
+<body class="page-blue">
+	
+	<!-- Encabezado y barra de navegación. -->
+	<?php include 'server/encabeza.php'; ?>
+	<!-- FIN Encabezado, barra navegación -->
+
+	<div class="content">
+		<div class="content-heading">
+			<div class="container">
+				<h1 class="heading">Festival Latinoamericano de Instalación de Software Libre 2015</h1>
+			</div>
+		</div>
+
+		<div class="content-inner">
+			<div class="container">
+				<p style="text-align: center"><img src="/img/logo_flisol.png" /></p>
+			</div>
+		</div>
+	</div>
+
+	<!-- Pie de página. -->
+	<?php include 'server/pie.php' ?>
+
+	<script src="/js/material/base.min.js"></script>
+
+</body>
+
+
+</html>
