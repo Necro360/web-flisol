7d0f63b3e5ada51dd8043e187ed53822f994968f
Guillermo Villafuerte <gvillafu@comunidad.unam.mx>
2015-04-12 19:53:25 -0500

    Modularización del encabezado y pie de página

---
+++ server/encabeza.php
@@ -0,0 +1,29 @@
+<header class="header">
+	<div class="clearfix pull-left">
+		<ul class="nav nav-list pull-left">
+			<li>
+				<a class="menu-toggle" href="#menu">
+					<span class="access-hide">Menú</span>
+					<span class="icon icon-menu"></span>
+					<span class="header-close icon icon-arrow-back"></span>
+				</a>
+			</li>
+		</ul>
+		<span class="header-logo"><a href="/">FLISoL Aragón 2015</a></span>
+	</div>
+</header>
+
+<nav class="menu" id="menu">
+	<div class="menu-scroll">
+		<div class="menu-wrap">
+			<div class="menu-content">
+				<ul class="nav">
+					<li><a href="/acercade">Acerca de</a></li>
+					<li><a href="/actividades/">Actividades</a></li>
+					<li><a href="/registro">Registro</a></li>
+					<li><a href="/contacto">Contacto</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</nav>
---
+++ server/pie.php
@@ -0,0 +1,6 @@
+<footer class="footer">
+	<div class="container">
+		<p>&copy; 2015, Comunidad de Desarrollo Aragón.<br />
+		La plantilla Material está disponible para su descarga desde <a target="_new" href="http://github.com/Daemonite/material">este repo de GitHub</a>.</p>
+	</div>
+</footer>
