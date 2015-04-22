e4bcf8f62d4c6af1351fa7d5dd833e410e8b76d7
Rodrigo Moreno <moreno.rdr@gmail.com>
2015-04-22 05:19:54 -0500

    + Archivo PHP con la página acerca de

---
+++ acerca.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+
+	<title>FLISoL Aragón 2015</title>
+	<meta name="description" content="Página inicial del FLISoL Aragón, a realizarse el 28 de abril de 2015." />
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1"/>
+	<link rel="stylesheet" href="/css/material/base.min.css" />
+
+	<!-- Open Graph Facebook -->
+	<meta property="og:url" content="http://flisolaragon.com.mx/" />
+	<meta property="og:type" content="website" />
+	<meta property="og:description" content="Página principal del FLISoL Aragón, a realizarse el 28 de abril de 2015." />
+	<meta property="og:name" content="FLISoL Aragón 2015" />
+	<meta property="og:image" content="http://flisolaragon.com.mx/img/logo_flisol.png" />
+
+	<style>
+		@media screen and (max-width: 480px) {
+			#logo {
+				width: 300px;
+			}
+		}
+	</style>
+	
+</head>
+
+<body class="page-blue">
+	
+	<!-- Encabezado y barra de navegación. -->
+	<?php include 'server/encabeza.php'; ?>
+	<!-- FIN Encabezado, barra navegación -->
+
+	<div class="content">
+		<div class="content-heading">
+			<div class="container">
+				<h1 class="heading">¿Qué es FLISoL?</h1>
+			</div>
+		</div>
+
+		<div class="content-inner">
+			<div class="container">
+				<p style="text-align: center">
+					<img id="logo" src="/img/logo_flisol.png" alt="Logo FLISoL" />
+				</p>
+				<h2 class="text-center">¿Qué es FLISoL?</h2>
+				<p>El Festival Latinoamericano de Instalación de Software Libre (FLISoL) es el evento de difusión de Software Libre más grande en Latinoamérica. Se realiza desde el año 2005 y desde el 2008 se adoptó su realización el 4to sábado de abril de cada año.</p>
+				<p>Su principal objetivo es promover el uso del software libre, dando a conocer al público en general su filosofía, alcances, avances y desarrollo.</p>
+				<p>A tal fin, las diversas comunidades locales de software libre (en cada país/ciudad/localidad), organizan simultáneamente eventos en los que se instala, de manera gratuita y totalmente legal, software libre en las computadoras que llevan los asistentes. Además, en forma paralela, se ofrecen charlas, ponencias y talleres, sobre temáticas locales, nacionales y latinoamericanas en torno al Software Libre, en toda su gama de expresiones: artística, académica, empresarial y social.<p>
+				
+				<h2 class="text-center">4to FLISoL FES Aragón</h2>
+				<p>El Festival Latinoamericano de Instalación de Software Libre (FLISoL) nace de las ganas de un grupo de alumnos apoyados en un ex alumno en difundir el conocimiento y el uso de herramientas de Software Libre. Su primera edición nace en 2012 con un aproximado de 23 horas de contenidos y el respectivo Install Fest. Su realización continuó para el 2013 con 28 horas de contenido y ahora en 2014 estamos por llevar a cabo la tercera edición en la Facultad de Estudios Superiores Aragón con un aproximado de 29 horas de contenido.</p>
+			</div>
+		</div>
+	</div>
+
+	<!-- Pie de página. -->
+	<?php include 'server/pie.php' ?>
+
+	<script src="/js/material/base.min.js"></script>
+
+</body>
+
+</html>
