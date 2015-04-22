d1b37a283a001a4b1af948e443dd562bfd5de079
Rodrigo Moreno <moreno.rdr@gmail.com>
2015-04-22 04:55:30 -0500

    + Google Analytics

--- server/pie.php
+++ server/pie.php
@@ -4,3 +4,13 @@
 		La plantilla Material está disponible para su descarga desde <a target="_new" href="http://github.com/Daemonite/material">este repo de GitHub</a>.</p>
 	</div>
 </footer>
+
+<script>
+  !function(F,l,I,s,O,L){F.GoogleAnalyticsObject=I;F[I]||(F[I]=function(){
+  (F[I].q=F[I].q||[]).push(arguments)});F[I].l=+new Date;O=l.createElement(s);
+  L=l.getElementsByTagName(s)[0];O.src='//www.google-analytics.com/analytics.js';
+  L.parentNode.insertBefore(O,L)}(window,document,'ga','script');
+
+  ga('create', 'UA-58762010-2', 'flisolaragon.com.mx');
+  ga('send', 'pageview');
+</script>
